65127eae84cedcfb25c24456dba5a48df2ed4f41
BatimusPrime <primebatimus@gmail.com>
2019-07-30 13:15:49 -0700

    refactoring from old repo pull

--- js/game.js
+++ js/game.js
@@ -7,99 +7,107 @@ var scorebox;
 var maddog;
 //falling obstacle
 var enemy;
+
 //arrays
-var targets = [];
-var ega = [];
+let targets = [];
+let shoots = [];
 //metrics
 var score = 0;
 var health = 519;
-var scum = 8;
-var afghan = [];
+
+//number of targets
+var tarNum = 8;
 
 function preload(){
-  //loads all images and assigns them variables
-  img = loadImage("/img/maddog.png");
-  imgTwo = loadImage("/img/iraq.png");
-  imgThree = loadImage("/img/ega.png");
-  imgFour = loadImage("/img/sadam.png");
-  imgFive = loadImage('/img/afghan.png');
+
+//loads all images and assigns them variables
+img = loadImage("/img/maddog.png");
+imgTwo = loadImage("/img/iraq.png");
+imgThree = loadImage("/img/ega.png");
+imgFour = loadImage("/img/sadam.png");
+imgFive = loadImage('/img/afghan.png');
 
 }
 
 function setup(){
 
-  createCanvas(700,600);
-  //creates new sprite
-  sprite = new Sprite();
-  //creates new falling object
-  enemy = new Enemy();
-  // creates target arrays
-  for (var i = 0; i < scum; i++){
-    targets[i] = new Target(i*75+50, 50);
-  }; 
+createCanvas(700,600);
 
+//creates new sprite
+sprite = new Sprite();
 
-  //healthbar display
-  healthbar = new Healthbar();
-  //score display
-  scorebox = new Scorebox();
-//setup
-};
+//creates new falling object
+enemy = new Enemy();
+
+// creates target arrays
+for (i = 0; i < tarNum; i++){
+targets[i] = new Target(i*75+50, 50);
+}; 
+
+//healthbar display
+healthbar = new Healthbar();
+
+//score display
+scorebox = new Scorebox();
+
+};// End setup
+
 
 function draw(){
 
-  background(51);
-  //shows sprite and calls move
-  sprite.show();
-  sprite.move();
-  //shows falling object (at random point on X axis) and calls update (to make it fall)
-  enemy.show();
-  enemy.update();
-  //shows stats.js
-  healthbar.show(300);
-  scorebox.show();
+background(51);
+
+//shows sprite and calls move
+sprite.show();
+sprite.move();
+//shows falling object (at random point on X axis) and calls update (to make it fall)
+enemy.show();
+enemy.update();
+//shows stats.js
+healthbar.show(300);
+scorebox.show();
 
 
 //displays remaining targets
-  for (var i=0; i < targets.length; i++){
-    targets[i].show();
+for (var i=0; i < targets.length; i++){
+targets[i].show(2);
 
-  };  
+};  
 //creates array of projectiles, their coordinates are set in the keypress function
-if(score < (scum*100)){for (var i=0; i < ega.length; i++){
-    ega[i].show();
-    ega[i].move();
-    //hit detection
-    for (var j=0; j<target.length; j++){
-      if (ega[i].hits(target[j])){
-        target[j].shrink();
-        ega[i].gone();
+if(score < (tarNum*100)){for (var i=0; i < shoots.length; i++){
+shoots[i].show();
+shoots[i].move();
+//hit detection
+for (var j=0; j<targets.length; j++){
+if (shoots[i].hits(targets[j])){
+targets[j].shrink();
+shoots[i].gone();
 
-      }; 
-  
-    };   
+}; 
+
+};   
 };
 
-}else if(score >= (scum * 100)){
+}else if(score >= (tarNum * 100)){
 
 //level 2
 
 
 };
-  for (var i=ega.length-1; i>=0; i--){
-    if (ega[i].toDelete){
+for (var i=shoots.length-1; i>=0; i--){
+if (shoots[i].toDelete){
 
-      ega.splice(i,1);
-    }
+shoots.splice(i,1);
+}
 };
 
 for (var i=targets.length-1; i>=0; i--){
-  if (targets[i].toDelete){
+if (targets[i].toDelete){
 
-    targets.splice(i,1);
-    score += 100;
-    console.log(score);
-  };
+targets.splice(i,1);
+score += 100;
+console.log(score);
+};
 
 };
 
@@ -119,83 +127,91 @@ if (health <= 0){
 noLoop();
 };
 
-  //draw
+//draw
 };
 
 //controls for sprite
 function keyReleased(){
-  if (key != ' '){
-  maddog.setDir(0);
+if (key != ' '){
+sprite.setDir(0);
 };
 };
 
 function keyPressed(){
-  if (key === ' '){
-    var egas = new Ega(maddog.x, height-130);
-    ega.push(egas);
+if (key === ' '){
+var shoot = new Shoot(sprite.x, height-130);
+shoots.push(shoot);
 
-  }
-  if (keyCode === RIGHT_ARROW){
-    maddog.setDir(1);
+}
+if (keyCode === RIGHT_ARROW){
+sprite.setDir(1);
 
-  } else if (keyCode === LEFT_ARROW){
-    maddog.setDir(-1);
+} else if (keyCode === LEFT_ARROW){
+sprite.setDir(-1);
 
 
-  }
+}
 
 
 }
 //TODO: Replace with class
 //constructor for targets
 function Target(x, y){
-  this.x = x;
-  this.y = y;
-  this.r = 30;
-  this.toDelete = false;
- 
+this.x = x;
+this.y = y;
+this.r = 30;
+this.toDelete = false;
 
-  this.shrink = function(){
-    if (this.r == 5){this.toDelete = true;}else{
-    this.r -= 5;
+
+this.shrink = function(){
+if (this.r == 5){this.toDelete = true;}else{
+this.r -= 5;
 };
-  };
+};
 
-  this.show = function(){
-    image(imgTwo, this.x, this.y, this.r*2, this.r*2);
-  }
+this.show = function(lvl){
+
+if (lvl == 1){
+image(imgTwo, this.x, this.y, this.r*2, this.r*2);
+}else if (lvl == 2){
+
+image(imgFive, this.x, this.y, this.r*2, this.r*2);
+
+
+}
+}
 
 };
 
 //Sprite constructor
 function Sprite(){
-  this.x = width/2;
-  this.y = height-125;
-  this.xdir = 0;
+this.x = width/2;
+this.y = height-125;
+this.xdir = 0;
 
 
 
 
-  this.show = function(){
+this.show = function(){
 
-    image(img, this.x, this.y, 75, 75);
-    this.x = constrain(this.x, 0,width - 80);
+image(img, this.x, this.y, 75, 75);
+this.x = constrain(this.x, 0,width - 80);
 
-  };
-  this.setDir = function(dir){
-    this.xdir = dir;
+};
+this.setDir = function(dir){
+this.xdir = dir;
 
 
-  }
-  this.move = function(dir){
-    this.x += this.xdir*5;
+}
+this.move = function(dir){
+this.x += this.xdir*5;
 
-  };
+};
 
-  this.stop = function(){
+this.stop = function(){
 this.r = .000001;
 
-  }
+}
 
 
 };
@@ -205,35 +221,37 @@ this.r = .000001;
 
 
 function Enemy() {
-  this.y = 0;
-  this.x = random(600);
+this.y = 0;
+this.x = random(600);
 
 
-  this.gravity = .35;
-  this.velocity = 0;
+this.gravity = .35;
+this.velocity = 0;
 
-  this.show = function() {
-    fill(255);
-    // ellipse(this.x, this.y, 32, 32);
-    image(imgFour, this.x, this.y, 50, 50);
-  }
+//call show function with arg determining image to display
+this.show = function() {
+fill(255);
+
+image(imgFour, this.x, this.y, 50, 50);
+
+}
 
 
 
-  this.update = function() {
-    this.velocity += this.gravity;
-    this.velocity *= 0.9;
-    this.y += this.velocity;
+this.update = function() {
+this.velocity += this.gravity;
+this.velocity *= 0.9;
+this.y += this.velocity;
 
-    if (this.y > height) {
-      this.y = 0;
-      this.x = random(600);
-    }
+if (this.y > height) {
+this.y = 0;
+this.x = random(600);
+}
 
-    if (this.y < 0) {
-      this.y = 0;
-      this.velocity = 0;
-    };
+if (this.y < 0) {
+this.y = 0;
+this.velocity = 0;
+};
 
 
 if (enemy.x < sprite.x + 50 && enemy.x + 50 > sprite.x && enemy.y < sprite.y + 50 && 50 + enemy.y > sprite.y){
@@ -241,15 +259,93 @@ if (enemy.x < sprite.x + 50 && enemy.x + 50 > sprite.x && enemy.y < sprite.y + 5
 return false;
 }
 
-  };
+};
 
 
 this.stop = function(){
 
 
-  this.y =10000;
-  this.gravity = 0;
-  this.velocity = 0;
+this.y =10000;
+this.gravity = 0;
+this.velocity = 0;
 };
 
 };
+
+
+//Score and health
+
+function Healthbar(){
+this.x = 100;
+this.y = height-51;
+
+
+this.show = function(){
+//draws rectangle with no outline
+noStroke();
+//red box rests behind green
+fill('red');
+rect(this.x, this.y, width-200 , 50);
+//health remaining
+fill('green');
+rect(this.x, this.y, health, 50);
+
+};
+
+
+
+
+};
+function Scorebox(){
+this.x = 80;
+this.y = height-51;
+this.show = function(){
+fill('white');
+textSize(25);
+text(score, 40, this.y+30);
+};
+
+}
+
+
+//projectile constructor 
+function Shoot(x, y){
+    this.x = x;
+    this.y = y;
+    this.toDelete = false;
+  
+    this.show = function(){
+      image(imgThree, this.x+20, this.y-20, 35,35);
+    };
+  this.gone = function(){
+  this.toDelete = true;
+  
+  
+  };
+    this.hits = function(target){
+  
+  
+      if (this.x < target.x + 50 && this.x + 50 > target.x && this.y < target.y + 50 && 50 + this.y > target.y){
+  
+        return true;
+  
+      }
+      else{
+  
+        return false;
+      };
+  
+  
+    };
+  
+    this.move = function(){
+      this.y = this.y-5;
+  
+  
+    };
+  
+  
+  
+    };
+  
+  
